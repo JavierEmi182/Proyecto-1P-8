c60c76799ecac02f4df8d8987c79e337c7fad67c
JavierEmi182 <javvespi@espol.edu.ec>
2020-11-13 20:10:38 -0500

    Base de clase Animal

---
+++ src/fundacion/Animal.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundacion;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Javier
+ */
+public class Animal {
+    private Date fechaIngreso;
+    private String nombre;
+    private String raza;
+    private String sexo;
+    private double peso;
+    private String observaciones;
+    private int edad;
+    private static int codigo=0000;
+    
+    
+}
