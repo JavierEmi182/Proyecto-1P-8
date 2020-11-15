21fa681d3aac85423a7d57b50411e0d92baad4a0
Josseline16 <joceastu@espol.edu.ec>
2020-11-14 23:30:15 -0500

    Clase Veterinaria y Clase Gasto Veterinaria

---
+++ src/fundacion/GastoVeterinaria.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundacion;
+import java.util.Date;
+/**
+ *
+ * @author user
+ */
+public class GastoVeterinaria {
+    private Animal animal;
+    private Date fecha;
+    private double monto;
+            
+}
---
+++ src/fundacion/Veterinaria.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package fundacion;
+
+/**
+ *
+ * @author user
+ */
+public class Veterinaria {
+    private String nombre;
+    private String contacto;
+    private String correo;
+}
