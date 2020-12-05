a66e50d46432665d4f9336bb1c6e25c2434a32eb
JavierEmi182 <javvespi@espol.edu.ec>
2020-12-04 20:15:59 -0500

    .jar

--- src/Default/Main.java
+++ src/Default/Main.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Main;
+package Default;
 
 import interfaz.FundacionUI;
 import java.io.IOException;
